ddb96a3c86e7723abfe160d765c7a914fcabf7d6
Matt Goatcher <matt@angcrest.net>
2016-01-07 15:10:48 -0800

    Enhance bootstrap default template

--- template.rb
+++ template.rb
@@ -115,24 +115,48 @@ create_file "app/views/layouts/application.html.haml" do
 !!!
 %html{:lang => "en"}
   %head
-    %meta{:charset => "utf-8"}/
-    %meta{:content => "IE=edge", "http-equiv" => "X-UA-Compatible"}/
-    %meta{:content => "width=device-width, initial-scale=1", :name => "viewport"}/
-    / The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags
-    %title Bootstrap 101 Template
-    / Bootstrap
-    %link{:href => "css/bootstrap.min.css", :rel => "stylesheet"}/
-    / HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
-    / WARNING: Respond.js doesn't work if you view the page via file://
-    /[if lt IE 9]
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    %meta{charset: "utf-8"}/
+    %meta{content: "IE=edge", "http-equiv": "X-UA-Compatible"}/
+    %meta{content: "width=device-width, initial-scale=1", name: "viewport"}/
+
+    %title = content_for(:title) || 'LocalBoy'
+
+    = stylesheet_link_tag 'application', media: 'all'
+    = javascript_include_tag 'application'
+    = csrf_meta_tags
   %body
-    %h1 Hello, world!
-    / jQuery (necessary for Bootstrap's JavaScript plugins)
-    %script{:src => "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"}
-    / Include all compiled plugins (below), or include individual files as needed
-    %script{:src => "js/bootstrap.min.js"}
+    %header
+      == render 'layouts/navigation'
+    %main {role: "main"}
+      #content-container
+        == bootstrap_flash
+        == yield
+    %footer
+      == render 'layouts/footer'
+  eos
+end
+create_file "app/views/layouts/navbar.html.haml"
+create_file "app/views/layouts/footer.html.haml"
+create_file "app/helpers/bootstrap_flash_helper.rb" do
+  <<-eos
+module BootstrapFlashHelper
+
+  def bootstrap_flash
+    flash_messages = []
+    flash.each do |type, message|
+      # Skip Devise :timeout and :timedout flags
+      next if type == :timeout
+      next if type == :timedout
+      type = :success if type == 'notice'
+      type = :danger  if ['alert', 'error'].include?(type)
+      text = content_tag(:div, 
+               content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+               message, :class => "text-center alert fade in alert-#{type}")
+      flash_messages << text if message
+    end
+    flash_messages.join("\n").html_safe
+  end
+end
   eos
 end
 
